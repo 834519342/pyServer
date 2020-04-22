64b156b3b68d5f96e248dba15524f12b6a1a4371
834519342 <834519342@qq.com>
2020-04-22 16:35:24 +0800

    代码文件

---
+++ mysql_manager.py
@@ -0,0 +1,137 @@
+
+import pymysql
+import json
+
+
+def connect_sql():
+    # 连接数据库
+    db = pymysql.connect("localhost", "root", "rootroot", "firstDB")
+    # 获取游标对象
+    cursor = db.cursor()
+    # 执行SQL命令
+    cursor.execute("SELECT VERSION()")
+    # 获取单条数据
+    data = cursor.fetchone()
+    print("Database version: %s" % data)
+    # 关闭数据库连接
+    db.close()
+
+
+def create_table():
+    db = pymysql.connect('localhost', 'root', 'rootroot', 'firstDB')
+    cursor = db.cursor()
+    # 如果表存在则删除
+    cursor.execute('DROP TABLE IF EXISTS sdk_config')
+
+    # 创建新表 id 自增主键
+    sql = '''CREATE TABLE sdk_config (
+            id int auto_increment primary key,
+            userID text,
+            permissions int,
+            data json)'''
+    cursor.execute(sql)
+
+    sql = 'alter table sdk_config auto_increment=1'
+    cursor.execute(sql)
+
+    db.close()
+
+
+def insert_data(user_id='1000', data='Null'):
+    if data == 'Null':
+        return
+    db = pymysql.connect('localhost', 'root', 'rootroot', 'firstDB')
+    cursor = db.cursor()
+    # 转json数据 ensure_ascii=False, 才可以包含非ascii字符\
+    json_str = json.dumps(data, ensure_ascii=False)
+    # print(json_str)
+    # 编辑权限
+    permissions = 1
+    if user_id == '1000':
+        permissions = 0
+    # 插入数据
+    sql = "insert into sdk_config(userID, data, permissions) VALUES ('%s', '%s', %d)" % (user_id, json_str, permissions)
+    try:
+        cursor.execute(sql)
+        # 提交到数据库
+        db.commit()
+        print('insert success')
+    except:
+        # 如果发生错误则回滚
+        print('insert error')
+        db.rollback()
+    db.close()
+
+
+def fetch_data(user_id='1000'):
+    db = pymysql.connect('localhost', 'root', 'rootroot', 'firstDB')
+    cursor = db.cursor()
+    data_arr = []
+    # 查询
+    sql = "select * from sdk_config where userID = '1000' or userID = '%s'" % user_id
+    try:
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        for row in results:
+            id = row[0]
+            userID = row[1]
+            permissions = row[2]
+            data = row[3]
+            dic = {'id': id, 'userID': userID, 'permissions': permissions, 'data': json.loads(data)}
+            data_arr.append(dic)
+            print("id = %d, userID = %s, permissions = %d data = %s" % (id, userID, permissions, json.loads(data)))
+    except:
+        print("Error: unable to fetch data")
+    db.close()
+    return data_arr
+
+
+def update_data(id=0, data=''):
+    if id == 0 or data == '':
+        return
+    db = pymysql.connect('localhost', 'root', 'rootroot', 'firstDB')
+    cursor = db.cursor()
+    # 更新
+    json_str = json.dumps(data, ensure_ascii=False)
+    sql = "update sdk_config set data = '%s' where id = %d and permissions = 1" % (json_str, id)
+    try:
+        cursor.execute(sql)
+        db.commit()
+    except:
+        db.rollback()
+    db.close()
+
+
+def delete_data(id=0):
+    if id == 0:
+        return
+    db = pymysql.connect('localhost', 'root', 'rootroot', 'firstDB')
+    cursor = db.cursor()
+    # 删除
+    sql = "delete from sdk_config where id = %d and permissions = 1" % id
+    try:
+        cursor.execute(sql)
+        db.commit()
+    except:
+        db.rollback()
+    db.close()
+
+
+if __name__ == '__main__':
+    # connect_sql()
+    # create_table()
+    #
+    # dic = {'name': '默认配置', 'snuser': 'appjiangcimangthreathuntercn', 'applicationId': 'default',
+    #        'collectURL': 'https://zqjBWuxbXzrk.yazx.com', 'snkey': '', 'version': '12',
+    #        'publicKey': '-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxHJS9F8OwaDDfJfEp1NfT7voRTOeWMDPPpFKvDkiB6UUmvoUCHv2LsNUYua5CREtP0m0c5UpkV9/o8cO8YcCZhqfVTUJjRMqaFU1BKSljI2ze2kc6F+IST9Y/JmodPzPe2+aOWsep3F1aMi2WZY70ldmB+3GwX4EKkZs36BHq2tWZmSVUwHSrGE0EaqEGfWWQFT3cHIHtDiuzm/3NYU9+J4KDN64mHAQxgAvjDMugUAob0atgfoB/6NByK+e1BgBtvOS4eHi/Pk2rjm2I+G6XWc1psW7BGW7BvjqDaddyi/rRVgSWfdDrgR17f2CtpPIMx10MVm8/Y0ytTXj/+upKQIDAQAB-----END PUBLIC KEY-----'}
+    # insert_data('1001', dic)
+
+    # dic = {'name': '默认配置', 'snuser': 'appjiangcimangthreathuntercn', 'applicationId': 'default',
+    #        'collectURL': 'https://zqjBWuxbXzrk.yazx.com', 'snkey': '', 'version': '12',
+    #        'publicKey': ''}
+    # update_data(2, dic)
+
+    # delete_data(3)
+    #
+    fetch_data('1001')
+
---
+++ server_sdkConfig.py
@@ -0,0 +1,198 @@
+
+import json, mysql_manager
+from wsgiref.simple_server import make_server
+from urllib.parse import parse_qs
+# 定义返回的数据
+dataArr = [
+    {
+        'name': '默认配置',
+        'snuser': 'appjiangcimangthreathuntercn',
+        'applicationId': 'default',
+        'publicKey': '-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxHJS9F8OwaDDfJfEp1NfT7voRTOeWMDPPpFKvDkiB6UUmvoUCHv2LsNUYua5CREtP0m0c5UpkV9/o8cO8YcCZhqfVTUJjRMqaFU1BKSljI2ze2kc6F+IST9Y/JmodPzPe2+aOWsep3F1aMi2WZY70ldmB+3GwX4EKkZs36BHq2tWZmSVUwHSrGE0EaqEGfWWQFT3cHIHtDiuzm/3NYU9+J4KDN64mHAQxgAvjDMugUAob0atgfoB/6NByK+e1BgBtvOS4eHi/Pk2rjm2I+G6XWc1psW7BGW7BvjqDaddyi/rRVgSWfdDrgR17f2CtpPIMx10MVm8/Y0ytTXj/+upKQIDAQAB-----END PUBLIC KEY-----',
+        'collectURL': 'https://zqjBWuxbXzrk.yazx.com',
+        'snkey': '',
+        'version': '1.0.0',
+    },
+    {
+        'name': 'Check token',
+        'snuser': 'appjiangcimangthreathuntercn',
+        'applicationId': 'default',
+        'publicKey': '',
+        'collectURL': 'https://rcapi.threathunter.cn',
+        'snkey': 'G@>r@#atxwbJt1f7-nRByQUBj@1UJ>ul',
+        'version': '1.0.0'
+    },
+    {
+        'name': '完美世界',
+        'snuser': 'TomdoXQ8shq7wVdU',
+        'applicationId': 'default',
+        'publicKey': '-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2NTQTGYVQ7EMJU+sjaY4Xp09MowG4lZpeyHLm0u14gVGFovbmXNJf35Xw+3NbsGGkr7VcTqEK8rFKSxeS6PHHDQiM66IZ4ge7d9itydaI2NrXy5X4U6KiIFh5VoTk5Uv8X/uUqLeAWTa1lHeY+8JbDKyweTUcnhDh0j/LErM1CBaUVJUF4h+JFnrJcIL+Zf+RG+VeAe9yDioleCiDDgeZ0Pe5n/6fC5mldiFhbT85wKFG7A80Gj2sbrlvzMybb1A9bttxXZOHtPqkCXse5g5td8opJfV+HtHMF2KM9wf41F8lwKU+FPo+JBVWNZ8C078NrxsDk1sRqPsLgjuJmBNxwIDAQAB-----END PUBLIC KEY-----',
+        'collectURL': 'http://182.61.172.131:7006',
+        'snkey': 'bpeN_ZF,oLonHM|Stu2m_dHjdPxA~7vl',
+        'version': '1.0.0'
+    }
+]
+
+
+def request_api(environ, start_response):
+    # 获取请求方式
+    request_method = environ['REQUEST_METHOD']
+    response_dic = {}
+    # 域名路径
+    path_info = environ['PATH_INFO']
+    print('PATH_INFO:', path_info)
+
+    # GET请求
+    if request_method == 'GET':
+        # 定义响应状态、响应数据的格式
+        status = '200 OK'
+        response_headers = [('Content-Type', 'text/html;charset=utf-8')]
+        # 处理数据
+        if path_info == '/get':
+            response_dic = path_get(environ)
+        else:
+            status = '404 pathError'
+            response_dic = {'error': '404'}
+        start_response(status, response_headers)
+
+    # POST请求
+    if request_method == 'POST':
+        # 定义响应状态、响应数据的格式
+        status = '200 OK'
+        response_headers = [('Content-Type', 'application/json')]
+        # 处理数据
+        if path_info == '/post':
+            response_dic = path_post(environ)
+        elif path_info == '/getAllConfig':
+            response_dic = get_all_config(environ)
+        elif path_info == '/insertConfig':
+            response_dic = insert_config(environ)
+        elif path_info == '/deleteConfig':
+            response_dic = delete_config(environ)
+        elif path_info == '/updateConfig':
+            response_dic = update_config(environ)
+        else:
+            status = '404 pathError'
+        start_response(status, response_headers)
+
+    return [json.dumps(response_dic).encode('utf-8')]
+
+
+# /get 路径处理
+def path_get(environ):
+    # 获取网址附带参数 域名/?num=[1-3]
+    params = parse_qs(environ['QUERY_STRING'])
+    print('params:', params)
+    value = params.get('num', [''])[0]
+    # 默认值
+    num = 0
+    # 判断是否位数字
+    if value.isdigit():
+        num = int(value)
+    if num > 0 and num <= len(dataArr):
+        arr = []
+        for index in range(0, num):
+            arr.append(dataArr[index])
+        return {'code': '200', 'data': arr}
+    return {'code': '200', 'data': dataArr}
+
+
+# /post 路径处理
+def path_post(environ):
+    # 获取请求附带的body参数 {num: [1-3]}
+    content_length = environ['CONTENT_LENGTH']
+    # 判断是否为数字
+    if content_length.isdigit():
+        body_length = int(content_length)
+        if body_length > 4:
+            # 获取请求参数
+            request_body = environ['wsgi.input'].read(body_length)
+            request_body = json.loads(request_body)
+            print('request_body:', request_body)
+            value = str(request_body['num'])
+            # 默认值
+            num = 0
+            if value.isdigit():
+                num = int(value)
+            if num > 0 and num <= len(dataArr):
+                arr = []
+                for index in range(0, num):
+                    arr.append(dataArr[index])
+                return {'code': '200', 'data': arr}
+    return {'code': '200', 'data': dataArr}
+
+
+# --------------------------  MySql数据库处理 -------------------------
+def get_all_config(environ):
+    # 获取请求附带的body参数 {num: [1-3]}
+    content_length = environ['CONTENT_LENGTH']
+    # 判断是否为数字
+    if content_length.isdigit():
+        body_length = int(content_length)
+        if body_length > 4:
+            # 获取请求参数
+            request_body = environ['wsgi.input'].read(body_length)
+            request_body = json.loads(request_body)
+            print('request_body:', request_body)
+            return {'code': '200', 'data': mysql_manager.fetch_data(request_body['userID'])}
+
+    return {'code': '-100', 'data': 'NULL'}
+
+
+def insert_config(environ):
+    # 获取请求附带的body参数 {num: [1-3]}
+    content_length = environ['CONTENT_LENGTH']
+    # 判断是否为数字
+    if content_length.isdigit():
+        body_length = int(content_length)
+        if body_length > 4:
+            # 获取请求参数
+            request_body = environ['wsgi.input'].read(body_length)
+            request_body = json.loads(request_body)
+            # print('request_body:', request_body)
+            mysql_manager.insert_data(request_body['userID'], request_body['data'])
+            return {'code': '200', 'data': mysql_manager.fetch_data(request_body['userID'])}
+
+    return {'code': '-100', 'data': 'NULL'}
+
+
+def delete_config(environ):
+    # 获取请求附带的body参数 {num: [1-3]}
+    content_length = environ['CONTENT_LENGTH']
+    # 判断是否为数字
+    if content_length.isdigit():
+        body_length = int(content_length)
+        if body_length > 4:
+            # 获取请求参数
+            request_body = environ['wsgi.input'].read(body_length)
+            request_body = json.loads(request_body)
+            print('request_body:', request_body)
+            mysql_manager.delete_data(request_body['id'])
+            return {'code': '200', 'data': mysql_manager.fetch_data(request_body['userID'])}
+
+    return {'code': '-100', 'data': 'NULL'}
+
+
+def update_config(environ):
+    # 获取请求附带的body参数 {num: [1-3]}
+    content_length = environ['CONTENT_LENGTH']
+    # 判断是否为数字
+    if content_length.isdigit():
+        body_length = int(content_length)
+        if body_length > 4:
+            # 获取请求参数
+            request_body = environ['wsgi.input'].read(body_length)
+            request_body = json.loads(request_body)
+            print('request_body:', request_body)
+            mysql_manager.update_data(request_body['id'], request_body['data'])
+            return {'code': '200', 'data': mysql_manager.fetch_data(request_body['userID'])}
+
+    return {'code': '-100', 'data': 'NULL'}
+
+
+if __name__ == '__main__':
+    ip = ''
+    port = 8000
+    http = make_server(ip, port, request_api)
+    print('serving http on port {0}...'.format(str(port)))
+    http.serve_forever()
